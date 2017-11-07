0a58eb7d6fb356034f4a23742149d65a15309912
Kaveon Smith <kaveon31@gmail.com>
2017-11-06 22:38:45 -0600

    fixed some bugs caused by extra headers

--- FileDownload.php
+++ FileDownload.php
@@ -8,10 +8,6 @@ $file = PROGRESS_PHOTO_PATH.$file_name;
 if(file_exists($file)) {
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="'.basename($file).'"');
-    header('Expires: 0');
-    header('Pragma: public');
-    header('Content-length: '.filesize($file));
     readfile($file);
     exit;
 }
